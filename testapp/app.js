8fe5b96340152d0d7dbb9f63efa19a44bedd5aab
test <kennkyuusituyou.mail@gmail.com>
2022-07-23 18:21:03 +0900

    ファイル整理

--- testapp/app.js
+++ testapp/app.js
@@ -39,3 +39,71 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+// 既に色々入っているので、そこへ追加
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+
+// signinページの追加
+var signinRouter = require('.routes/signin');
+app.use('/signin');
+
+// session, passport.initialize, passport.sessionは以下の順番で追加
+app.use(session({
+  secret: "testing",
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// authentication
+passport.serializeUser(function(username, done) {
+  console.log('serializeUser');
+  done(null, username);
+});
+
+passport.deserializeUser(function(username, done) {
+  console.log('deserializeUser');
+  done(null, {name:username});
+});
+
+passport.use(new LocalStrategy(
+  {
+    // signinのformで定義したnameの要素をセット
+    usernameField: "username",
+    passwordField: "password"
+  },
+  function(username, password, done){
+    // ここでは、データベースを使わずに、仮にusernameとpasswordを固定で入れています。
+    if(username == "test" && password == 123456789){
+      return done(null, username);
+    }
+    return done(null, false, {message: "invalid"});
+  }
+));
+
+app.post('/signin',
+  passport.authenticate('local',
+    {
+      failureRedirect: "/signin"
+    }
+  ),
+  function(req, res, next){
+    // res.redirect("/")でreq.userが渡せなかったので、ここでfetchを使っています。
+    // https://github.com/jaredhanson/passport/issues/244
+    // fetchは以下のようにインストール
+    // npm install --save isomorphic-fetch
+    // var fetch = require('isomorphic-fetch');
+    fetch("http://localhost:3000/signin",
+      {
+        credentials: "include"
+      }
+    ).then(function(){
+      res.redirect("/");
+    }).catch(function(e){
+      console.log(e);
+    });
+  }
+);
