4654a4b8f6b479ad7b43ab5455829161ba46ed21
test <kennkyuusituyou.mail@gmail.com>
2022-09-02 15:25:01 +0900

    ゲーム終了時の処理実装中

--- app/app.js
+++ app/app.js
@@ -5,13 +5,12 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = 3000;
-const hostname = 'localhost'; //hostname指定
+const hostname = 'localhost'; // hostname指定
 
 let number = 0; // カウンター
-let users = [];
-
-const timeInterval = 10000; //コード交換時間10秒(仮、今だけ)
-const timeLimit = 60 * 60 * 1000;    //ゲーム終了時間1時間(仮)
+let users = []; // ユーザ情報保存配列
+const timeInterval = 10000;          // コード交換時間10秒(仮、今だけ)
+const timeLimit = 20000;    // ゲーム終了時間1時間(仮), test20秒
 
 // database処理
 const mysql = require('mysql');
@@ -25,17 +24,16 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-//問題と回答取得
+// 問題と回答取得
 let kadai = '';
 let result = '';
 connection.query('SELECT task, result FROM test ORDER BY RAND() LIMIT 1', function (error, response) {
     if (error) throw error;
 
-    kadai = response[0].task;    //問題取得
-    result = response[0].result; //回答取得
+    kadai = response[0].task;    // 問題取得
+    result = response[0].result; // 回答取得
     console.log(response);
 });
-
 connection.end();
 
 // socket接続確立中の処理
@@ -48,20 +46,20 @@ io.on('connection', function (socket) {
     number++;
 
     // クライアントからのイベントによる処理
-    //start処理
+    // start処理
     socket.on('client_to_server_start', () => {
         io.emit("start", kadai);
-        //コード交換のタイムインターバル処理
+        // コード交換のタイムインターバル処理
         setInterval(function () {
             io.emit('server_to_client_timenews');
         }, timeInterval);
-        /*ゲーム終了タイマー,今は停止させている
-        setTimeout(function(){
-
-        },timeLimit);*/
+        // ゲーム終了タイマー
+        setTimeout(function () {
+            io.emit("server_to_client_end");
+        }, timeLimit);
     });
 
-    //ユーザからコードデータを受信し、他のユーザに送信する処理
+    // ユーザからコードデータを受信し、他のユーザに送信する処理
     socket.on("client_to_server_code", function (data) {
         let toId = data.id + 1;
         if (toId > socket.client.conn.server.clientsCount) {
@@ -71,10 +69,15 @@ io.on('connection', function (socket) {
     });
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));       //postフォームで配列のデータを受け取らないようにしている
+// ゲーム終了処理
+/*function gameEnd() {
 
-//ページ遷移処理、フォーム受信処理
+}*/
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));       // postフォームで配列のデータを受け取らないようにしている
+
+// ページ遷移処理、フォーム受信処理
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
@@ -84,6 +87,10 @@ app.post('/code', function (req, res) {
     user.name = req.body.name;
     users.push(user);
     res.sendFile(__dirname + '/views/code.html');
+});
+
+app.get('/result', function(req, res){
+    res.sendFile(__dirname + '/view/result.html');
 });
 
 server.listen(port, hostname, function () {
