375f081e38dea95ee509980ac39c4b04f85f5eca
test <kennkyuusituyou.mail@gmail.com>
2022-11-16 23:30:18 +0900

    コード簡略化リファクタリング

--- app/public/js/func.js
+++ app/public/js/func.js
@@ -28,40 +28,7 @@ function sleep(a) {
     return;
 }
 
-/*
-async function run() {
-    const code = editor.getValue(); // エディタに書いたソースコード読み取り
-    const input = document.getElementById("input").value;
-
-    var params1 = {
-        source_code: code,
-        language: 'c',
-        input: input,
-        api_key: 'guest'
-    };
-    const query_params1 = new URLSearchParams(params1);
-    const options1 = {
-        method: "post",
-        body: query_params1
-    };
-
-    const res_create = await fetch("http://api.paiza.io/runners/create", options1);
-    const res_create_json = await res_create.json();
-    console.log(res_create_json.id);
-    sleep(2000);
-
-    url = "http://api.paiza.io/runners/get_details?id=" + res_create_json.id + "&api_key=guest";
-
-    const res_get_details = await fetch(url);
-    const res_get_details_json = await res_get_details.json();
-    if(res_get_details_json.build_result == 'success'){
-        $('#output').text(res_get_details_json.stdout);
-    }else{
-        $('#output').text(res_get_details_json.build_stderr);
-    }
-    console.log(res_get_details_json.stdout);
-}*/
-
+// paiza.ioのapiを使用したコンパイル関数
 const language = ["c", "cpp", "java", "python", "ruby", "php", "go", "javascript"];
 
 async function compile(code, lang, input) {
